ca491ae3a4f5d45cb6a30a93efdf37452332f394
Jens Ritter <jens.ritter@gmail.com>
2021-09-05 08:12:13 +0200

    removed test-module "zform-test"

--- zform-test/src/main/resources/static/js/template-dialog.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    "use strict";
-    const name = document.getElementById("name");
-    name.addEventListener("click", function () {
-        $('#dlg').modal('show');
-    }, false);
-});
