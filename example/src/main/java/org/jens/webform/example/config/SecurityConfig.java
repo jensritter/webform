b92c38aa5fcbb08f146e765070166c479a051f44
Jens Ritter <jens.ritter@gmail.com>
2022-06-08 15:15:39 +0200

    Finales Update auf SpringSecurity

--- example/src/main/java/org/jens/webform/example/config/SecurityConfig.java
+++ example/src/main/java/org/jens/webform/example/config/SecurityConfig.java
@@ -2,19 +2,19 @@ package org.jens.webform.example.config;
 
 import org.jens.shorthand.security.config.ShorthandSecurityDefaults;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * @author Jens Ritter on 16/03/2021.
  */
-@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain web(HttpSecurity http) throws Exception {
-        return ShorthandSecurityDefaults.ignoreUrls(http, "/**");
+        return ShorthandSecurityDefaults.ignoreChain(http, "/**", "/");
     }
 
 }
